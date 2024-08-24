79999eafd4fb2b9587a71b7a27120e42843cea18
Kaique-GM <kaiquegmoficial@gmail.com>
2024-08-24 17:53:46 -0300

    Implementing possible moves of Rook

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -23,28 +23,39 @@ public class ChessMatch {
 		}
 		return mat;
 	}
-	public ChessPiece perfomChessMove(ChessPosition sourcePosition,ChessPosition targetPosition) {
+
+	public ChessPiece perfomChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition(source);
-		Piece capturedPiece = makeMove(source,target);
-		return (ChessPiece)capturedPiece;
+		validateTargetPosition(source, target);
+		Piece capturedPiece = makeMove(source, target);
+		return (ChessPiece) capturedPiece;
 	}
-	private Piece makeMove(Position source,Position target) {
+
+	private Piece makeMove(Position source, Position target) {
 		Piece p = board.removePiece(source);
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		return capturedPiece;
 	}
+
 	private void validateSourcePosition(Position position) {
-		if(!board.thereIsAPiece(position)){
+		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is not a piece on source position");
 		}
-		if(!board.piece(position).isThereAnyPossibleMove()) {
+		if (!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible moves for the chosen piece");
 		}
-		
+
 	}
+
+	private void validateTargetPosition(Position source, Position target) {
+		if (!board.piece(source).possibleMove(target)) {
+			throw new ChessException("The chosen piece can't move to target position");
+		}
+	}
+
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 	}
@@ -63,5 +74,6 @@ public class ChessMatch {
 		placeNewPiece('e', 7, new Rook(board, Color.BLACK));
 		placeNewPiece('e', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('d', 8, new King(board, Color.BLACK));
+
 	}
 }
